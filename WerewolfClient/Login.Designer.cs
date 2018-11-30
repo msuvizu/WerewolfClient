d7f3a33e8d58a22eaa8cfb40ab19612bead02e25
msuvizu <31123769+msuvizu@users.noreply.github.com>
2018-11-30 14:33:45 +0700

    Revert "merge"

--- WerewolfClient/Login.Designer.cs
+++ WerewolfClient/Login.Designer.cs
@@ -36,62 +36,54 @@
             this.BtnSignIn = new System.Windows.Forms.Button();
             this.BtnSignUp = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.TbGame = new System.Windows.Forms.ComboBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.BtnCharacter = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBServer = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("TH Sarabun New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(697, 399);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 158);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 32);
+            this.label1.Size = new System.Drawing.Size(57, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "User name";
+            this.label1.Text = "Login";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Font = new System.Drawing.Font("TH Sarabun New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(709, 438);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 200);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 32);
+            this.label2.Size = new System.Drawing.Size(92, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             // 
             // TbLogin
             // 
-            this.TbLogin.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.TbLogin.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TbLogin.ForeColor = System.Drawing.Color.Sienna;
-            this.TbLogin.Location = new System.Drawing.Point(800, 402);
+            this.TbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbLogin.Location = new System.Drawing.Point(132, 158);
             this.TbLogin.Name = "TbLogin";
-            this.TbLogin.Size = new System.Drawing.Size(320, 35);
+            this.TbLogin.Size = new System.Drawing.Size(273, 29);
             this.TbLogin.TabIndex = 2;
             // 
             // TbPassword
             // 
-            this.TbPassword.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.TbPassword.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TbPassword.ForeColor = System.Drawing.Color.Sienna;
-            this.TbPassword.Location = new System.Drawing.Point(800, 441);
+            this.TbPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbPassword.Location = new System.Drawing.Point(132, 200);
             this.TbPassword.Name = "TbPassword";
             this.TbPassword.PasswordChar = '*';
-            this.TbPassword.Size = new System.Drawing.Size(320, 35);
+            this.TbPassword.Size = new System.Drawing.Size(273, 29);
             this.TbPassword.TabIndex = 3;
             // 
             // BtnSignIn
             // 
-            this.BtnSignIn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnSignIn.BackgroundImage")));
-            this.BtnSignIn.Font = new System.Drawing.Font("TH Sarabun New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnSignIn.Location = new System.Drawing.Point(910, 480);
+            this.BtnSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSignIn.Location = new System.Drawing.Point(248, 246);
             this.BtnSignIn.Name = "BtnSignIn";
-            this.BtnSignIn.Size = new System.Drawing.Size(100, 45);
+            this.BtnSignIn.Size = new System.Drawing.Size(94, 45);
             this.BtnSignIn.TabIndex = 4;
             this.BtnSignIn.Text = "Sign In";
             this.BtnSignIn.UseVisualStyleBackColor = true;
@@ -99,76 +91,51 @@
             // 
             // BtnSignUp
             // 
-            this.BtnSignUp.BackColor = System.Drawing.Color.Transparent;
-            this.BtnSignUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnSignUp.BackgroundImage")));
-            this.BtnSignUp.Font = new System.Drawing.Font("TH Sarabun New", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnSignUp.Location = new System.Drawing.Point(800, 480);
+            this.BtnSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSignUp.Location = new System.Drawing.Point(132, 246);
             this.BtnSignUp.Name = "BtnSignUp";
-            this.BtnSignUp.Size = new System.Drawing.Size(100, 45);
+            this.BtnSignUp.Size = new System.Drawing.Size(94, 45);
             this.BtnSignUp.TabIndex = 5;
             this.BtnSignUp.Text = "Sign Up";
-            this.BtnSignUp.UseVisualStyleBackColor = false;
+            this.BtnSignUp.UseVisualStyleBackColor = true;
             this.BtnSignUp.Click += new System.EventHandler(this.BtnSignUp_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(800, 20);
+            this.pictureBox1.Location = new System.Drawing.Point(174, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(320, 320);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 93);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
-            // TbGame
+            // label3
             // 
-            this.TbGame.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.TbGame.Font = new System.Drawing.Font("TH Sarabun New", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
-            this.TbGame.ForeColor = System.Drawing.Color.Sienna;
-            this.TbGame.FormattingEnabled = true;
-            this.TbGame.Items.AddRange(new object[] {
-            "2 Player",
-            "4 Player",
-            "16 Player"});
-            this.TbGame.Location = new System.Drawing.Point(800, 360);
-            this.TbGame.Name = "TbGame";
-            this.TbGame.Size = new System.Drawing.Size(320, 36);
-            this.TbGame.TabIndex = 12;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "API Address";
             // 
-            // label4
+            // TBServer
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("TH Sarabun New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(732, 360);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(62, 32);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "Player";
-            // 
-            // BtnCharacter
-            // 
-            this.BtnCharacter.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnCharacter.BackgroundImage")));
-            this.BtnCharacter.Font = new System.Drawing.Font("TH Sarabun New", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCharacter.Location = new System.Drawing.Point(1020, 480);
-            this.BtnCharacter.Name = "BtnCharacter";
-            this.BtnCharacter.Size = new System.Drawing.Size(100, 45);
-            this.BtnCharacter.TabIndex = 14;
-            this.BtnCharacter.Text = "Character";
-            this.BtnCharacter.UseVisualStyleBackColor = true;
-            this.BtnCharacter.Click += new System.EventHandler(this.BtnHowTo_Click);
+            this.TBServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBServer.Location = new System.Drawing.Point(132, 115);
+            this.TBServer.Name = "TBServer";
+            this.TBServer.Size = new System.Drawing.Size(273, 29);
+            this.TBServer.TabIndex = 8;
+            this.TBServer.Text = "http://localhost:2343/werewolf/";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.MenuBar;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1904, 1041);
-            this.Controls.Add(this.BtnCharacter);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.TbGame);
+            this.ClientSize = new System.Drawing.Size(422, 328);
+            this.Controls.Add(this.TBServer);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.BtnSignUp);
             this.Controls.Add(this.BtnSignIn);
@@ -176,13 +143,8 @@
             this.Controls.Add(this.TbLogin);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.ForeColor = System.Drawing.Color.Sienna;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Login";
             this.Text = "Login";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -198,8 +160,7 @@
         private System.Windows.Forms.Button BtnSignIn;
         private System.Windows.Forms.Button BtnSignUp;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox TbGame;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button BtnCharacter;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBServer;
     }
 }
